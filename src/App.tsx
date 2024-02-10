8e059dcb5e03de39c8c6565ae2b381678d5364eb
Ahmed Alaa <Ahmed@gmail.com>
2024-02-10 22:55:45 +0200

    init project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Card from "./components/Card";
+
+function App() {
+  return (
+    <>
+      <div className="sm:container">
+        <Card/>
+      </div>
+    </>
+  );
+}
+
+export default App;
